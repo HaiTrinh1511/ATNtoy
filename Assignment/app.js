ed175663b5e05226d4e3ebf2d97322f893f4c33d
HaiTrinh1511 <67938967+HaiTrinh1511@users.noreply.github.com>
2020-08-18 09:38:16 +0700

    commit
    second commit

--- Assignment/app.js
+++ Assignment/app.js
@@ -1,8 +1,9 @@
-//var http = require('http');
+var http = require('http');
 
 const express = require('express');
 const engines = require('consolidate');
 const app = express();
+var fs = require('fs')
 
 var bodyParser = require("body-parser");
 const { ObjectID } = require('mongodb');
@@ -53,6 +54,15 @@ app.get('/remove', async (req,res)=>{
     res.redirect('/');
 })
 
+app.get('/removeuser', async (req,res)=>{
+    let id = req.query.id;
+    var ObjectID = require('mongodb').ObjectID
+    let client = await MongoClient.connect(url);
+    let dbo = client.db("ProductDB");
+    await dbo.collection("User").deleteOne({_id:ObjectID(id)});
+    res.redirect('/');
+})
+
 app.get('/doSearch', async(req,res)=>{
     let name_search = req.query.txtSearch;
     let client= await MongoClient.connect(url);
@@ -65,5 +75,59 @@ app.get('/contact', async(req,res)=>{
     res.render('contact');
 })
 
-const PORT = process.env.PORT || 3000;
+// test register
+app.get('/register', (req,res)=>{
+    res.render('register');
+})
+
+var fileName = 'users.txt';
+app.post('/doRegister', async function(req, res){
+    let inputName = req.body.txtName;
+    let inputEmail = req.body.txtEmail;
+    let data = inputName + '------' +inputEmail + '\n';
+    fs.appendFile(fileName,data,function(err){
+        res.redirect('/');
+    })
+    let client = await MongoClient.connect(url);
+    let dbo = client.db("ProductDB");
+    await dbo.collection("User").insertOne({name:inputName,email:inputEmail});
+    res.redirect("/");
+})
+app.get('/allUser', async function(req,res){
+    // let result = [];
+    // fs.readFile(fileName, 'utf8', function(error,data){
+    //     let allUsers = data.split("\n");
+    //     for(i=0;i<allUsers.length;i++){
+    //         if(allUsers[i].trim().length != 0){
+    //             let nameX = allUsers[i].split('------')[0];
+    //             let emailX = allUsers[i].split('------')[1];
+    //             result.push({name:nameX,email:emailX})
+    //         }
+    //     }
+    //     // res.render('allUser',{model:result});
+    // })
+    let client = await MongoClient.connect(url);
+    let dbo = client.db("ProductDB");
+    let result = await dbo.collection("User").find({}).toArray();
+    res.render('allUser', {model:result});
+})
+
+var PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+
+// app.post('/doRegister',function(req,res){
+//     let inputName = req.body.txtName;
+//     let inputEmail = req.body.txtEmail;
+//     //check data before writing to file
+//     if(inputName.length <4){
+//         let errorModel = {nameError: "Ten phai lon hon 3 ky tu!"
+//             ,emailError:"email k hop le"};
+//         res.render('register',{model:errorModel})
+//     }else{
+//         let data = inputName + ';' +inputEmail + '\n';
+//             fs.appendFile(fileName,data,function(err){
+//             res.redirect('/');
+//         })
+//     }
+// })
